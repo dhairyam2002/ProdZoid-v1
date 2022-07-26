72f6ee3113d5fc8a873e3c295732e0c90991a5bc
Dhairya <dhairyam2002@gmail.com>
2022-07-26 13:32:22 +0530

    Updated Footer

--- front-end/src/components/Layout/Footer/Footer.js
+++ front-end/src/components/Layout/Footer/Footer.js
@@ -1,21 +1,28 @@
 import React from "react";
-import { Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import "./Footer.css"
+import GitHubIcon from '@mui/icons-material/GitHub';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import Twitter from "@mui/icons-material/Twitter";
 const Footer = function () {
-    return(
+    return (
         <div className="footer">
             <div className="left-s">
-                <Link className = "linkf" to = "/about">About Us</Link>
-                <Link className = "linkf" to = "/contact">Contact Us</Link>
+                <Link className="linkf" to="/about">About Us</Link>
+                <Link className="linkf" to="/contact">Contact Us</Link>
             </div>
             <div className="middle-s">
-                <Link to = "/" className="logof linkf">PRODUCT ZOID</Link>
-                <p>Copyrights 2021 © Dhairya</p>
+                <Link to="/" className="logof linkf logo">ProdZoid</Link>
+                <p>Copyrights 2022 © Dhairya</p>
             </div>
             <div className="right-s">
-                <a className = "linkf" href="https://facebook.com">Facebook</a>
-                <a className = "linkf" href="twitter.com">Twitter</a>
-                <a className = "linkf" href="instagram.com">Instagram</a>
+                <div>
+                    <a className="linkf" href="https://github.com/dhairyam2002"><GitHubIcon className="icon" />Github</a>
+                </div>
+                <div>
+                    <a className="linkf" href="twitter.com"><Twitter className="logo" />Twitter</a>
+                </div>
+
             </div>
         </div>
     )
