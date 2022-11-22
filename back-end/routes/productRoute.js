68fc14ffaa30361208fb3f65846054709460c4d5
Dhairya Modi <dhairyam2002@gmail.com>
2022-11-23 00:11:11 +0530

    search by categories-sort by relevance + show similar products recommendations

--- back-end/routes/productRoute.js
+++ back-end/routes/productRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getAllProducts, createNewProduct, updateProduct, deleteProduct, getProductDetails, createProductReview, deleteReview, getAllReviews} = require('../controllers/productController');
+const { getAllProducts, createNewProduct, updateProduct, deleteProduct, getProductDetails, createProductReview, deleteReview, getAllReviews, getProductByCategories} = require('../controllers/productController');
 
 
 const { isAuthenticatedUser, authorizeRole } = require('../middleware/auth');
@@ -18,5 +18,7 @@ router.route("/admin/product/:id").put(isAuthenticatedUser,authorizeRole("admin"
 router.route("/product/:id").get(getProductDetails);
 
 router.route("/product/review").put(isAuthenticatedUser,createProductReview).delete(isAuthenticatedUser, deleteReview).post(getAllReviews);
+
+router.route('/product/categories/search').post(getProductByCategories);
 
 module.exports = router;
